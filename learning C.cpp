0ca0251818bb4fed6688c7fa19d1c66ce6846d0e
Damian Lujan <damianlujan9@icloud.com>
2026-04-13 11:09:10 -0500

    Add basic C++ program with variable examples

---
+++ learning C.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main(){
+
+    // Integer (whole numbers)
+    int age = 19;
+    int year = 2026;
+    int days = 1;  
+
+
+    // double (numbers with decimal points)
+    double price = 29.99;
+    double gpa = 3.3;
+    double temperature = 85.7;
+
+
+    // char (single characters)
+
+    char grade = 'A';
+    char initial = 'D';
+    char currency = '$';
+
+
+
+    
+    // bool (true or false)
+
+    bool student = false;
+    bool ishot = true;
+    bool iscloseby = false;
+
+
+
+    // string (objects that represent a sequence of text or characters)
+    std::string name = "Damian Lujan";
+    std::string city = "Seattle";
+    std::string strain = "Sativa";
+    std::string address = "Fee J Cassleman";
+
+    std::cout << "My name is " << name << " and I am " << age << " years old." << std::endl;
+
+    
+
+    return 0;
+}
