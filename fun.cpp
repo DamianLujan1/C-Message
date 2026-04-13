e41a2e7310e355a32d9a7210d44191d771a6dd9e
Damian Lujan <damianlujan9@icloud.com>
2026-04-13 15:50:47 +0000

    1ne

---
+++ fun.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+
+    // This is a simple code
+    std::cout << "My name is Damian!" << std::endl;
+    std::cout << "It's really hot outside" << '\n';
+    return 0;
+}
